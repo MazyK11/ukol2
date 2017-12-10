a8c076405f5dea030f69fb77eb7c879999842202
MazyK11 <33461811+MazyK11@users.noreply.github.com>
2017-12-10 18:48:52 +0100

    opravy 2

--- ukol2/src/ukol2/Ukol2.java
+++ ukol2/src/ukol2/Ukol2.java
@@ -22,42 +22,39 @@ public class Ukol2 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-//      do proměnné delka si uložím počet parametrů v args 
-        int delka = args.length; 
-        int c = nacteni1radku(args,delka);
-//      vytvoření pole p o velikosti počtu řádků * počet sloupců
-        double p[] = new double[c*3];
-        nactenizbytku(args,delka,p,c);  
-//      proměnná exp je defaultně nastavená na 2
-        double exp = 2;
-        exp = parametr(args,exp);
-
-//      vytvoření polí, které reprezentují načtené hodnoty - pro 
-//      větší přehlednost
-        double x[] = new double [c];
-        double y[] = new double [c];
-        double value[] = new double [c];
-//      naplnění polí z pole p
-        for(int i =0; i < c;i++){
-            x[i] = p[i*3];
-            y[i] = p[(i*3)+1];
-            value[i] = p[(i*3)+2];
+//      ošetření proti pádu bez argumentů
+        if (args.length  <= 1){
+            System.out.print("Nebyly zadány parametry vstupu a výstupu");
+            System.exit(-1);
         }
+        int radky = nacteni1radku(args);
+//      vytvoření pole p o velikosti počtu řádků * počet sloupců
+        double x[] = new double [radky];
+        double y[] = new double [radky];
+        double value[] = new double [radky];
+        
+        nactenizbytku(args,radky,x,y,value);
+
+//      proměnná exp je defaultně nastavená na 2
+        double exponent = 2;
+        exponent = parametr(args,exponent);
+
+
         
 //      Vytvoření proměnných, které reprezentují mřížku
         double barsx []= new double[100];
         double barsy []= new double [100];
-        maxmin(p,c,barsx,barsy);
+        maxmin(x,y,radky,barsx,barsy);
 
 //      Vytvoření konečného pole save, do kterého se uloží výsledná tabulka
         double result [] = new double[100*100];
-        distance(x,y,barsx,barsy,result,value,c,exp);
+        distance(x,y,barsx,barsy,result,value,radky,exponent);
 
 //      Zápis do výstupního souboru
         PrintWriter a;
         try {
             Locale.setDefault(Locale.US);
-            a = new PrintWriter(args[delka-1]);
+            a = new PrintWriter(args[args.length-1]);
             for(int i =0;i < 100;i++){
                 for(int j =0;j<100;j++){
                     a.format("%.2f ;", result[j+(i*100)]);
@@ -69,7 +66,7 @@ public class Ukol2 {
             a.close();
         }
         catch(FileNotFoundException ex){
-            System.out.print("Soubor nebyl nalezen\n");
+            System.out.format("Soubor %s nebyl nalezen\n",args[args.length-1]);
             System.exit(-1);
         }
         
@@ -81,21 +78,22 @@ public class Ukol2 {
 //  první cyklus vypočte koeficient k, druhý cyklus použije vypočtený koeficient
 //  a vytvoří vážené vzdálenosti, třetí cyklus vypočte výslednou hodnotu
 //  interpolovaného bodu
-    public static double idw(double dist[],double value[],int c, double exp){
+    public static double idw(double dist[],double value[],int radky, 
+            double exponent){
 //      Vytvoření pomocných proměnných
-        double weightdist[] = new double[c];
-        double helpdist[]= new double [c];
+        double weightdist[] = new double[radky];
+        double helpdist[]= new double [radky];
         double truevalue = 0;
         double k =0;
-        for (int i =0; i < c;i++){
-            k =k + (1/Math.pow(dist[i],exp));
-            helpdist[i] = (1/Math.pow(dist[i],exp));
+        for (int i =0; i < radky;i++){
+            k =k + (1/Math.pow(dist[i],exponent));
+            helpdist[i] = (1/Math.pow(dist[i],exponent));
         }
-        for (int i = 0; i< c;i++){
+        for (int i = 0; i< radky;i++){
             weightdist[i] = helpdist[i] * (1/k);
             
         }
-        for (int i =0;i < c;i++){
+        for (int i =0;i < radky;i++){
             truevalue = truevalue + (weightdist[i] * value[i]); 
         }
         return truevalue;
@@ -108,9 +106,9 @@ public class Ukol2 {
 //  uložena do pole save. Při vzdálenosti 0 je bodu přiřazena hodnota bodu
 //  vstupního - jedná se o ten samí bod
     public static void distance(double x[], double y[],double
-            barsx [],double barsy [],double result[], double value[],int c,
-            double exp){
-        double dist[]= new double [c];
+            barsx [],double barsy [],double result[], double value[],int radky,
+            double exponent){
+        double dist[]= new double [radky];
         int u =0;
         int m =0;
         for (int j =0;j<(100+1);j++){
@@ -122,7 +120,7 @@ public class Ukol2 {
                 break;
             }
             int count =  0;
-            for(int i = 0; i < c;i++) {
+            for(int i = 0; i < radky;i++) {
                 dist[i] = Math.sqrt((x[i] - barsx[j])*(x[i] - barsx[j]) + 
                         (y[i] - barsy[u])*(y[i] -barsy[u]));
                 if (dist[i] == 0){
@@ -134,58 +132,66 @@ public class Ukol2 {
             if(result[m] == value[count]){
             }
             else {
-            result[m]= idw(dist,value,c,exp);
+            result[m]= idw(dist,value,radky,exponent);
             }
             m++;
         }
     }
-//  Metoda, která převede načtená čísla na hodnoty, které jim odpovídají
-//  "4" = 4 Vstupy: pole p pro převedené hodnoty, pole items s načtenými
-//  hodnotami a pomocná proměnná k
-//  Výstup: nic 
-    public static void parse(String []items,double []p,int k){
-        for(int i = 0;i < items.length;i++){
-            p[i+k] = Double.parseDouble(items[i]);
-        }
-    }
+    
 //  Metoda, která najde minimum a maximu načtených souřadnic x, a následně  
 //  naplní pole barsx hodnotami se stejným rozdílem od min do max. Potom 
 //  proces zopakuje pro souřadnice y
 //  Vstup: pole p, počet řádků c, pole mřížových souřadnic x a y 
 //  Výstup: nic
-    public static void maxmin(double p[],int c,double barsx [],double barsy [])
+    public static void maxmin(double x[],double y[],int radky,
+            double barsx [],double barsy [])
     {
+        double max = 0;
+        double min = 0;
         for(int j =0;j<2;j++){
-            double max =p[j];
-            for(int i = 0;i < c;i++){
-                if( max >= p[(i*3)+j]){ 
+            if(j ==0){
+                max =x[j];
+                min= x[j];
+                for(int i = 0;i < radky;i++){
+                    if( max >= x[i]){ 
+                    }
+                    else{
+                    max = x[i];
+                    }
+                    if( min <= x[i]){ 
+                    }
+                    else{
+                    min = x[i];
+                    }
                 }
-                else{
-                max = p[(i*3)+j];
-                }
-            }
-            double min = p[j];
-            for(int i = 0;i < c;i++){
-                if( min <= p[(i*3)+j]){ 
-                }
-                else{
-                    min = p[(i*3)+j];
-                }
-            }
-            double r = max- min;
-            r = r/(100-1);
-            if (j==0){
+                double r = max- min;
+                r = r/(100-1);
                 barsx[0] = min;
                 for(int m = 1;m < 100;m++){
                     barsx[m] = barsx[m-1] + r;
                 }
-            }
-            else {
+            }  
+            else{
+                max = y[j-1];
+                min = y[j-1];
+                for(int i = 0;i < radky;i++){
+                    if( max >= y[i]){ 
+                    }
+                    else{
+                    max = y[i];
+                    }
+                    if( min <= y[i]){ 
+                    }
+                    else{
+                    min = y[i];
+                    }
+                }
+                double r = max- min;
+                r = r/(100-1);
                 barsy[0] = min;
                 for(int m = 1;m < 100;m++){
                     barsy[m] = barsy[m-1] + r;
-
-                }
+                } 
             }
         }
     }
@@ -195,34 +201,34 @@ public class Ukol2 {
 //      Vstup: delka parametrů, parametry a exponent
 //      Výstup: hodnota exponentu
 //      pokud bude zadán nekorektní vstup program použije defaultní exponent 2
-    public static double parametr(String[] args,double exp){
+    public static double parametr(String[] args,double exponent){
         try {
             for(int i = 0;i < args.length;i++){
                 if("-p".equals(args[i])){
-                    exp = Double.parseDouble(args[i+1]);
+                    exponent = Double.parseDouble(args[i+1]);
                 }
-                if (exp <= 0){
+                if (exponent <= 0){
                     System.out.print("exponent nemůže být menší nebo rovno 0, "
                         + "bude použit defaultní exponent 2");
-                    exp = 2;
+                    exponent = 2;
                 }
-                return exp;
+                return exponent;
             }
         }
         catch(NumberFormatException ex){
             System.out.print("nalezeny chybné znaky, bude použit defaultní"
                     + " exponent 2\n");
-            exp = 2;
+            exponent = 2;
         }
-        return exp;
+        return exponent;
     }
 //      try blok pro zjištění počtu řádků, catch zachytává všechny možné výjimky
 //      a ukončuje program přes System.exit(-1)
 //      Vytvoření proměnné c, do které uložím počet řádků
-    public static int nacteni1radku(String[] args, int delka){
+    public static int nacteni1radku(String[] args){
         try { 
             BufferedReader vstup = new BufferedReader(new FileReader
-            (args[delka-2]));
+            (args[args.length-2]));
             String r = vstup.readLine();
             String [] roww = r.split(",");
             return Integer.parseInt(roww[0]);
@@ -232,7 +238,7 @@ public class Ukol2 {
             System.exit(-1);
         }
         catch(FileNotFoundException ex){
-            System.out.print("Soubor nebyl nalezen\n");
+            System.out.format("Soubor %s nebyl nalezen\n",args[args.length-2]);
             System.exit(-1);
         }
         catch(IOException ex){
@@ -244,20 +250,24 @@ public class Ukol2 {
 //      try - na načtení řádků a naplnění pole items 
 //      následné volání metody, která parsuje načtené hodnoty a naplní jimi 
 //      pole p
-    public static void nactenizbytku(String[] args, int delka, double p[],
-            int c){
+//  Metoda, která převede načtená čísla na hodnoty, které jim odpovídají
+//  "4" = 4 
+    public static void nactenizbytku(String[] args,int radky, double x[],
+            double y[],double value[]){
         try{
             BufferedReader vstup = new BufferedReader(new FileReader
-            (args[delka-2]));
+            (args[args.length-2]));
             String row;
             String [] items;
             int k = 0;
             vstup.readLine();
-            for(int i=0;i < c;i++){
+            for(int i=0;i < radky;i++){
                 row = vstup.readLine();
                 items = row.split(",");
-                parse(items,p,k);
-                k = k + 3;
+                x[k] = Double.parseDouble(items[0]);
+                y[k] = Double.parseDouble(items[1]);
+                value[k] = Double.parseDouble(items[2]);
+                k++;
             }
         }
         catch(NumberFormatException ex){
@@ -265,7 +275,7 @@ public class Ukol2 {
                 System.exit(-1);
         }
         catch(FileNotFoundException ex){
-            System.out.print("Soubor nebyl nalezen\n");
+            System.out.format("Soubor %s nebyl nalezen\n",args[args.length-2]);
             System.exit(-1);
         }
         catch(IOException ex){
